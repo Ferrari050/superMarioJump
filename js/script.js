66724b2d895f3369c9897054fa1c2bb39ead6ff1
Ferrari050 <jvm.ferrari@hotmail.com>
2024-01-23 11:33:53 -0300

    arrumando tudo

--- js/script.js
+++ js/script.js
@@ -12,6 +12,15 @@ document.addEventListener('click', function() {
       }, 500);
     });
 
+document.addEventListener('keydown', function() {
+    titulo.style.color = 'transparent';
+    titulo.style.background = 'none';
+    var mario = document.getElementById('mario');        mario.classList.add('jump');
+    setTimeout(function() {
+        mario.classList.remove('jump');
+          }, 500);
+    });
+
     const loop = setInterval(() => {
         
         const pipePosition = pipe.offsetLeft;
