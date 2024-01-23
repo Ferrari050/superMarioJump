a0ea8647464572ce67fa10fd732de08ea901737f
Ferrari050 <jvm.ferrari@hotmail.com>
2024-01-23 16:30:56 -0300

    adicionando score ao jogo

--- js/script.js
+++ js/script.js
@@ -2,6 +2,15 @@ const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 const titulo = document.querySelector('.titulo');
 
+//contador
+var contador = 0;
+var pipeCompleteAnimation = document.getElementById('pipe');
+
+pipeCompleteAnimation.addEventListener('animationiteration', function() {
+  contador++;
+  document.getElementById('display-score').textContent = 'Score: ' + contador;
+});
+
 document.addEventListener('click', function() {
     titulo.style.color = 'transparent';
     titulo.style.background = 'none';
@@ -22,23 +31,28 @@ document.addEventListener('keydown', function() {
           }, 1000);
     });
 
+//loop para checagem se o jogo acabou ou nao
+    
     const loop = setInterval(() => {
         
+        //criando a integracao html js
         const pipePosition = pipe.offsetLeft;
         const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
         
+        //condição para acabar o jogo
+
         if (pipePosition <= 110 && pipePosition > 0 && marioPosition < 80){
+            mario.src = './images/game-over.png';
             pipe.style.animation = 'none'
             pipe.style.left = `${pipePosition}px`;
-
             mario.style.animation = 'none'
             mario.style.bottom = `${marioPosition}px`;
-            
-            mario.src = './images/game-over.png';
-            mario.style.width = '75px';
+            mario.style.width = '75px';  
             mario.style.marginLeft = '50px';
-        
             clearInterval (loop);
+            alert('Você morreu! sua pontuação : ' + contador)
+            location.reload()
+            
         }
-
     }, 10);
+
