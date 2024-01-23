5fec8dfc09c196a219ce50653a842e582ee5ec71
Ferrari050 <jvm.ferrari@hotmail.com>
2024-01-23 11:48:59 -0300

    teste

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ document.addEventListener('keydown', function() {
         const pipePosition = pipe.offsetLeft;
         const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
         
-        if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 100){
+        if (pipePosition <= 110 && pipePosition > 0 && marioPosition < 80){
             pipe.style.animation = 'none'
             pipe.style.left = `${pipePosition}px`;
 
